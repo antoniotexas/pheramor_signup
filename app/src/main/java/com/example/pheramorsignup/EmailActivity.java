6a83b2e29fad1f52c325d55b53dc1d137481ac29
Jose Ramos <joseramos@joses-mbp.attlocal.net>
2018-08-02 09:46:27 -0500

    implemented email activity and layout

---
+++ app/src/main/java/com/example/pheramorsignup/EmailActivity.java
@@ -0,0 +1,71 @@
+package com.example.pheramorsignup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EmailActivity extends AppCompatActivity {
+
+    EditText emailField;
+    Button   nextBtn;
+    String   email;
+    Toolbar  toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        emailField = findViewById(R.id.email_field);
+        nextBtn    = findViewById(R.id.next_button);
+
+        setToolbar();
+        sendData();
+    }
+
+    public void setToolbar(){
+        toolbar = findViewById(R.id.navigation_bar);
+        setTitle("Sign Up");
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.dark_blue));
+    }
+
+    public boolean isEmailValid(){
+        boolean valid = true;
+        email = emailField.getText().toString();
+
+        if (TextUtils.isEmpty(email) ) {
+            emailField.setError("Should not be empty");
+            valid = false;
+        }else if(!email.contains("@") || !email.contains(".com") ){
+            emailField.setError("Please enter an email");
+            valid = false;
+        }
+        else {
+            emailField.setError(null);
+        }
+        return valid;
+    }
+
+    public void sendData(){
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isEmailValid()) {
+                    return;
+                }
+                Intent intent = new Intent(getBaseContext(), PasswordActivity.class);
+                intent.putExtra("email",email);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/pheramorsignup/MainActivity.java
+++ app/src/main/java/com/example/pheramorsignup/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.pheramorsignup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signUpButn;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setToolbar();
+
+        signUpButn = findViewById(R.id.sign_up_btn);
+
+        signUpButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), EmailActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void setToolbar(){
+        toolbar = findViewById(R.id.navigation_bar);
+        setTitle("Pheramor");
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.dark_blue));
     }
 }
